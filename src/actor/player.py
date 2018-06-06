63abd266a8aeb3970b931e42c5056f29d88df940
Peter Morgan <peter.morgan@kineticsocial.com>
2018-06-05 21:45:05 -0400

    make client "private"

--- src/actor/player.py
+++ src/actor/player.py
@@ -13,7 +13,7 @@ class Player(BaseActor):
         # This might not be needed - we can probably check
         # if the object is of subclass Player or NPC instead
         self.is_player = True
-        self.client = client
+        self._client = client
 
         # Ability to perform various tasks or skills
         # Some abilities are granted by class/race
@@ -22,17 +22,17 @@ class Player(BaseActor):
 
     def send(self, msg):
         """Send a message to player, supports color codes"""
-        self.client.send_cc(msg)
+        self._client.send_cc(msg)
 
 
     def send_wrapped(self, msg):
         """Send wrapped text to player, supports color codes"""
-        self.client.send_wrapped(msg)
+        self._client.send_wrapped(msg)
 
 
     def send_raw(self, msg):
         """Send raw string to player, no color support"""
-        self.client.send_raw(msg)         
+        self._client.send_raw(msg)         
 
 
     def add_ability(self, ability):
