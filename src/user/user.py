0b10b73ef271932185776c3dd224c061e7f7b044
Peter Morgan <peter.morgan@kineticsocial.com>
2018-06-05 21:47:07 -0400

    Add in user command handling

--- src/user/user.py
+++ src/user/user.py
@@ -13,8 +13,42 @@ class User(BaseUser):
     """
 
     def __init__(self, client):
-        BaseUser.__init__(client)
+        log.debug('Inside User.__init__()')
+        BaseUser.__init__(self, client)
         # Set of allowed commands
         self.commands = set()
 
 
+    def add_command(self, command):
+        log.debug('Adding "{}" command to player {}'.format(command, self._profile['name']))
+        self._commands.add(command)
+
+
+    def remove_command(self, command):
+        log.debug('Removing "{}" command from player {}'.format(command, self._profile['name']))
+        self._commands.remove(command)
+
+
+    def clear_commands(self):
+        log.debug('Removing all commands from player {}'.format(self._profile['name']))
+        self._commands = set()
+
+
+    def has_command(self, command):
+        if command in self._commands:
+            return True
+        else:
+            return False
+
+
+    def list_commands(self):
+        return list(self._commands)
+
+
+    def _state_playing(self):
+        """User command interpreter"""
+        line = self._client.get_command()
+        command, args = line.split(' ')
+        log.debug('USER INPUT: {} -> {}'.format(command, args))
+        self.player.send('{} -> {}'.format(command, args))
+        
