4bfcbd94d7dc4f2354aa1677b362def1a422a6a5
Peter Morgan <smartattack@gmail.com>
2018-07-02 23:29:09 -0400

    create GameState before instantiating Player

--- test/test_player.py
+++ test/test_player.py
@@ -7,27 +7,32 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from actor.player import Player
+from database.game_state import GameState
 import globals as GLOBALS
 from utils import log
 
 
 def test_player_name():
+    GLOBALS.game_state = GameState()
     p = Player()
     assert p.name == 'nobody'
 
 def test_player_set_name():
+    GLOBALS.game_state = GameState()
     name = 'Testname'
     p = Player()
     p.name = name
     assert p.name == name
 
 def test_player_attributes():
+    GLOBALS.game_state = GameState()
     p = Player()
 
     assert p.get_attribute('Baloney') == None
 
 
 def test_player_set_attributes():
+    GLOBALS.game_state = GameState()
     p = Player()
     p.update(strength=17)
     assert p.get_attribute('strength') == 17
